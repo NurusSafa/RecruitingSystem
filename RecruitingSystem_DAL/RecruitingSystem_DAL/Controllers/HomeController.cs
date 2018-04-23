0bb7e3101b34720011cfa799720fe33b1954083d
NurusSafa <Nurus Safa@SAFA-PC>
2018-04-23 20:08:46 +0600

    CoreTemplate Apps Selected

---
+++ RecruitingSystem_DAL/RecruitingSystem_DAL/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecruitingSystem_DAL.Models;
+
+namespace RecruitingSystem_DAL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Safas contact page.";
+
+            return View();
+        }
+
+        public IActionResult ViewPage()
+        {
+            ViewData["Message"] = "Safas contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RecruitingSystem_DAL/RecruitingSystem_DAL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecruitingSystem_DAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
